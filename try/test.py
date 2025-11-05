848e4e400b87aea0c9ab3dffc1b4113d8c15a2d3
Kulan3 <nutthanondeeoam@gmail.com>
2025-11-06 08:19:16 +0900

    try 6

--- try/test.py
+++ try/test.py
@@ -1,8 +1,3 @@
-#  _____       _  _        ___ __   __
-# |_   _| ___ | || | ___  / __|\ \ / /
-#   | |  / -_)| || |/ _ \| (__  \   /
-#   |_|  \___||_||_|\___/ \___|  \_/
-
 import cv2
 from ultralytics import YOLO
 from djitellopy import Tello
@@ -13,10 +8,10 @@ tello.connect()
 print("Battery:", tello.get_battery())
 tello.streamon()
 
-# Load YOLOv8 model
+
 model = YOLO('bp.pt', task='detect')
 
-# Get the latest frame reader
+
 frame_read = tello.get_frame_read(with_queue=False, max_queue_len=0)
 
 try:
@@ -26,20 +21,15 @@ try:
             print("No frame received")
             continue
 
-        # Run detection
         results = model(frame)
 
-        # Draw annotations (Ultralytics returns BGR suitable for cv2.imshow)
         annotated = results[0].plot()
 
-        # Show window
         cv2.imshow("YOLOv8 Tello Drone Tracking", annotated)
 
-        # Press 'x' to exit
         if cv2.waitKey(1) & 0xFF == ord('x'):
             break
 
 finally:
-    # Clean up
     tello.end()
     cv2.destroyAllWindows()
